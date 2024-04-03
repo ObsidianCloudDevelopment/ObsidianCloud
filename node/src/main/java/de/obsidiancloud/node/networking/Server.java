190913af44c748a9c42e19b27f00420504430b22
Fabi.exe <66797305+FabiPunktExe@users.noreply.github.com>
2024-04-03 11:23:47 +0200

    Removed netty

--- node/src/main/java/de/obsidiancloud/node/networking/Server.java
+++ node/src/main/java/de/obsidiancloud/node/networking/Server.java
@@ -1,12 +1,18 @@
 package de.obsidiancloud.node.networking;
 
+import java.io.Closeable;
 import java.io.IOException;
 import java.nio.channels.ServerSocketChannel;
 
-public class Server {
+public class Server implements Closeable {
     private final ServerSocketChannel serverSocket;
 
     public Server(String bindAddress, int port) throws IOException {
-        this.serverSocket = ServerSocketChannel.open();
+        serverSocket = ServerSocketChannel.open();
+    }
+
+    @Override
+    public void close() throws IOException {
+        serverSocket.close();
     }
 }
